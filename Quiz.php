04bd8faf53dfc7f090384377fa458b1da34e2a01
b185j406 <78661382+b185j406@users.noreply.github.com>
2021-04-24 10:57:00 -0700

    Create Quiz.php

---
+++ Quiz.php
@@ -0,0 +1,20 @@
+<?php
+	text fields
+	$ans1 = $_POST["ans1"];
+	$ans2 = $_POST["ans2"];
+	$ans3 = $_POST["ans3"];
+
+	echo "Question 1: What is 2+2?<br>";
+	echo "You answered: " . $ans1 . "<br>";
+	echo "Correct answer: fish<br><br>";
+	
+	echo "Question 2: What is 7+7?<br>";
+	echo "You answered: " . $ans2 . "<br>";
+	echo "Correct answer: triangle<br><br>";
+
+	echo "Question 3: What is 6+6?<br>";
+	echo "You answered: " . $ans2 . "<br>";
+	echo "Correct answer: bunny<br><br>";
+
+	echo "Your score: 0%<br>;
+?>
